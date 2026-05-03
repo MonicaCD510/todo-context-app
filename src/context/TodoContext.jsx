5d559a6be0a11b3e52f1a63233bf8f809927324c
Monica Davila <monica.davila510@gmail.com>
2026-05-03 10:59:01 -0500

    Add todos state and TodoProvider

--- src/context/TodoContext.jsx
+++ src/context/TodoContext.jsx
@@ -1,3 +1,13 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const TodoContext = createContext();
+
+export const TodoProvider = ({ children }) => {
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <TodoContext.Provider value={{ todos, setTodos }}>
+      {children}
+    </TodoContext.Provider>
+  );
+};
