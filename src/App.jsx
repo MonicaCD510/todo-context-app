3abdbdc43af2e164a85aef0687ec84d027073b14
Monica Davila <monica.davila510@gmail.com>
2026-05-03 11:19:09 -0500

    Wrap App with TodoProvider

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { TodoProvider } from "./context/TodoContext";
 
 function App() {
   const [count, setCount] = useState(0)
