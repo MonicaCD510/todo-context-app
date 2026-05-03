8cc4407fe621a9eae4eb7318d6708c7549a097e4
Monica Davila <monica.davila510@gmail.com>
2026-05-03 16:48:44 -0500

    Fix TaskList render structure

--- src/context/TaskList.jsx
+++ src/context/TaskList.jsx
@@ -4,18 +4,19 @@ import { TodoContext } from "../../context/TodoContext";
 function TaskList() {
   const { todos } = useContext(TodoContext);
 
-  return (
-    <div>
-      <h2>Task List</h2>
+return (
+  <div>
+    <h2>Task List</h2>
 
-      {todos.map((todo) => (
-        <div key={todo.id}>
-          {todo.text}
-        </div>
-      ))}
+    {todos.map((todo) => (
+      <div key={todo.id}>
+        {todo.text}
+      </div>
+    ))}
 
-    </div>
-  );
+  </div>
+); 
 }
+
 
 export default TaskList;
